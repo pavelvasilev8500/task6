4a136aa91d2f1e34c886bbbc4aa4b5fd0c90f606
pavelvasilev8500 <61418223+pavelvasilev8500@users.noreply.github.com>
2020-04-08 22:24:08 +0300

    Create Program.cs

---
+++ task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empoyees empoyees = new Empoyees();
+            empoyees.metod();
+        }
+    }
+}
